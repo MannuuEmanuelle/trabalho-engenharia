aba1fb8c4b0690faa602fdca29e0a48d5c753f5f
Emanuelle <capic.2024118tads0027@aluno.ifpi.edu.br>
2025-08-12 08:52:41 -0300

    Classe vendedor

--- Vendedor.java
+++ Vendedor.java
@@ -1,5 +1,11 @@
 public class Vendedor {
+    private String nome;
     private InterfaceComissao comissao;
+    
+    public Vendedor(String nome, InterfaceComissao comissao){
+        this.nome = nome;
+        this.comissao = comissao;
+    }
 
     public void setComissao (InterfaceComissao comissao){
         this.comissao = comissao;
@@ -8,4 +14,17 @@ public class Vendedor {
     public double calcularComissao(double valor){
         return comissao.calcular(valor);
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public InterfaceComissao getComissao() {
+        return comissao;
+    }
+    
 }
